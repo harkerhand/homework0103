54c6647dca80a41e4630b036a3162253afb091f3
harkerhand <harkerhand@outlook.com>
2024-02-19 15:35:42 +0800

    进行接口实现分离

---
+++ 001/exp01/Complex.cpp
@@ -0,0 +1,68 @@
+/*
+ * @Date: 2024-02-19 14:13:52
+ * @LastEditors: harkerhand harkerhand@outlook.com
+ * @LastEditTime: 2024-02-19 14:15:28
+ * @FilePath: \homework2\001\exp01\Complex.cpp
+ */
+
+#include "Complex.h"
+#include <ostream>
+using std::ostream;
+
+Complex::Complex()
+    : realPart(0.0), imaginaryPart(0.0)
+{
+}
+
+Complex::Complex(double r, double i)
+    : realPart(r), imaginaryPart(i)
+{
+}
+
+Complex::Complex(const Complex &c)
+    : realPart(c.realPart), imaginaryPart(c.imaginaryPart)
+{
+}
+
+Complex::~Complex()
+{
+    // 析构函数
+}
+
+double Complex::getReal() const
+{
+    return realPart;
+}
+
+double Complex::getImag() const
+{
+    return imaginaryPart;
+}
+
+void Complex::setReal(double r)
+{
+    realPart = r;
+}
+
+void Complex::setImag(double i)
+{
+    imaginaryPart = i;
+}
+
+Complex Complex::operator+(const Complex &c) const
+{
+    Complex temp(realPart + c.realPart, imaginaryPart + c.imaginaryPart);
+    return temp;
+}
+
+Complex Complex::operator-(const Complex &c) const
+{
+    Complex temp(realPart - c.realPart, imaginaryPart - c.imaginaryPart);
+    return temp;
+}
+
+ostream &operator<<(ostream &out, const Complex &c)
+{
+    out << "<" << c.realPart << ", " << c.imaginaryPart << ">";
+    return out;
+}
--- 001/exp01/Complex.h
+++ 001/exp01/Complex.h
@@ -1,15 +1,12 @@
 /*
  * @Date: 2024-02-19 14:33:06
  * @LastEditors: harkerhand harkerhand@outlook.com
- * @LastEditTime: 2024-02-19 14:52:05
+ * @LastEditTime: 2024-02-19 14:15:49
  * @FilePath: \homework2\001\exp01\Complex.h
  */
 
 #ifndef COMPLEX_H
 #define COMPLEX_H
-
-#include <ostream>
-using std::ostream;
 
 class Complex
 {
@@ -32,62 +29,5 @@ public:
     friend ostream &operator<<(ostream &os, const Complex &c);
 };
 
-Complex::Complex()
-    : realPart(0.0), imaginaryPart(0.0)
-{
-}
-
-Complex::Complex(double r, double i)
-    : realPart(r), imaginaryPart(i)
-{
-}
-
-Complex::Complex(const Complex &c)
-    : realPart(c.realPart), imaginaryPart(c.imaginaryPart)
-{
-}
-
-Complex::~Complex()
-{
-    // 析构函数
-}
-
-double Complex::getReal() const
-{
-    return realPart;
-}
-
-double Complex::getImag() const
-{
-    return imaginaryPart;
-}
-
-void Complex::setReal(double r)
-{
-    realPart = r;
-}
-
-void Complex::setImag(double i)
-{
-    imaginaryPart = i;
-}
-
-Complex Complex::operator+(const Complex &c) const
-{
-    Complex temp(realPart + c.realPart, imaginaryPart + c.imaginaryPart);
-    return temp;
-}
-
-Complex Complex::operator-(const Complex &c) const
-{
-    Complex temp(realPart - c.realPart, imaginaryPart - c.imaginaryPart);
-    return temp;
-}
-
-ostream &operator<<(ostream &out, const Complex &c)
-{
-    out << "<" << c.realPart << ", " << c.imaginaryPart << ">";
-    return out;
-}
 
 #endif
