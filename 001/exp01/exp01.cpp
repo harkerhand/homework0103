a3655026ef5e0a00d6fafc567816606908c7d3c3
HarkerHand <harkerhand@outlook.com>
2024-02-26 18:02:34 +0800

    测试代码同步实验手册

--- 001/exp01/exp01.cpp
+++ 001/exp01/exp01.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    Complex c1(1.0, 2.0), c2(3.0, 4.0);
+    Complex c1(1, 7), c2(9, 2), c3(10, 1), c4(11, 5);
     cout << c1 << " + " << c2 << " = " << c1 + c2 << endl;
-    cout << c1 << " - " << c2 << " = " << c1 - c2 << endl;
+    cout << c3 << " - " << c4 << " = " << c3 - c4 << endl;
 }
