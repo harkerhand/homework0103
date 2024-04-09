9a89185860f75a93b15652c3db1f4cde51c43070
HarkerHand <harkerhand@outlook.com>
2024-04-09 22:22:00 +0800

    实现了文件和屏幕输出

--- 003/exp06/exp06.cpp
+++ 003/exp06/exp06.cpp
@@ -1,5 +1,6 @@
 #include "Shape.h"
 #include <iostream>
+#include <fstream>
 #include "shapes.h"
 
 using namespace std;
@@ -11,9 +12,17 @@ int main()
     Square s1({3, 3}, 4);
     Circle c2 = s1.incircle();
 
+    // 实现了文件和屏幕输出
+    ofstream fout("output.txt");
+    c1.print(fout);
     c1.print();
+    t1.print(fout);
     t1.print();
+    r1.print(fout);
     r1.print();
+    s1.print(fout);
     s1.print();
+    c2.print(fout);
     c2.print();
+    fout.close();
 }
