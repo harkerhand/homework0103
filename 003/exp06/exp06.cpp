d6d6ec2f3784dc234b11b6b9dd82e723c4f9a740
HarkerHand <harkerhand@outlook.com>
2024-04-09 22:28:15 +0800

    完善

--- 003/exp06/exp06.cpp
+++ 003/exp06/exp06.cpp
@@ -13,16 +13,21 @@ int main()
     Circle c2 = s1.incircle();
 
     // 实现了文件和屏幕输出
-    ofstream fout("output.txt");
-    c1.print(fout);
+    ofstream fout("output.txt", ios::trunc | ios::out);
+    if (!fout.is_open())
+        cout << "error" << endl;
+    else
+    {
+        c1.print(fout);
+        t1.print(fout);
+        r1.print(fout);
+        s1.print(fout);
+        c2.print(fout);
+        fout.close();
+    }
     c1.print();
-    t1.print(fout);
     t1.print();
-    r1.print(fout);
     r1.print();
-    s1.print(fout);
     s1.print();
-    c2.print(fout);
     c2.print();
-    fout.close();
 }
